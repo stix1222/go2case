2ad873604e67b3e7e083edbf2d51aa542d990620
s.fells <scfells1@gmail.com>
2022-04-16 14:16:32 -0400

    page update

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,37 @@ import city_image from '../public/city_view.jpg';
 function HomePage() {
   return (
   <div className=''>
-    <div className="bg-image container"></div>
+    <div className="bg-image"></div>
     <div className="intro">
       <div className="spacer"></div>
       <h1 className="">Welcome to Case Technologies</h1>
       <hr className="container"/>
       <h3 className="start-career">Where your success is our only objective</h3>
     </div>
+    <div className="homeStatement">
+      <h1>WHAT WE DO</h1>
+    </div>
     <div className="Group1">
-      <div className="leftSideTop"></div>
-      <div className="rightSideTop"></div>
+      <div className="leftSideTop">
+        <h1 className="leftSideInfo">Automate outdated job functions</h1>
+      </div>
+      <div className="rightSideTop">
+        <h1 className="rightSideInfo">Innovation and cutting edge Technologies</h1>
+      </div>
     </div>
     <div className="Group2">
-      <div className="leftSide"></div>
-      <div className="rightSide"></div>
+    <div className="bottomLeftLeft">
+        <h1 className="leftSideInfo">Custom Software Applications</h1>
+      </div>
+      <div className="bottomLeftRight">
+        <h1 className="rightSideInfo">Serverless Technologies</h1>
+      </div>
+      <div className="bottomRightLeft">
+        <h1 className="leftSideInfo">Cloud Computing</h1>
+      </div>
+      <div className="bottomRightRight">
+        <h1 className="rightSideInfo">Containerization and microservices</h1>
+      </div>
     </div>
     <div>
     </div>
